933b590f90c7663687f0f484ecb0adbad850de4b
Panitou <joaco0mr4@gmail.com>
2024-09-11 20:41:57 -0500

    feat ✅ modificaciones en el archivo astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,12 @@
 import { defineConfig } from "astro/config";
 
-import node from "@astrojs/node";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
 import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercelServerless(),
-
-  adapter: node({
-    mode: "standalone",
-  }),
-
+  adapter: vercelServerless(), // Usas el adaptador de Vercel para desplegar en Serverless
   integrations: [react(), tailwind()],
 });
