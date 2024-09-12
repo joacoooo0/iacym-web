5053157598da6ccddefe885c00f6269063d1f336
Panitou <joaco0mr4@gmail.com>
2024-09-11 20:29:46 -0500

    feat ✅ add dependencie vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
+  adapter: vercelServerless(),
 
   adapter: node({
-    mode: 'standalone'
+    mode: "standalone",
   }),
 
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
 });
